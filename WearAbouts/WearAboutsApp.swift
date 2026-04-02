232208af24b92993e47867bce07ae6e8fa523792
Emma Ebeling <ebelinge@msu.edu>
2026-04-02 09:05:20 -0400

    Initial Commit

---
+++ WearAbouts/WearAboutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WearAboutsApp.swift
+//  WearAbouts
+//
+//  Created by Emma Ebeling on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WearAboutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
